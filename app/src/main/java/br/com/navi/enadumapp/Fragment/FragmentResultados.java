da1f806e42b1955b47c2ae159a546234208a3d9c
VictorMonte <victorbcm@hotmail.com>
2016-08-19 19:52:36 -0300

    Implementação da Home
    Correção do texto da fragment

--- app/src/main/java/br/com/navi/enadumapp/Fragment/FragmentResultados.java
+++ app/src/main/java/br/com/navi/enadumapp/Fragment/FragmentResultados.java
@@ -16,6 +16,6 @@ public class FragmentResultados extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ranking, container, false);
+        return inflater.inflate(R.layout.fragment_resultados, container, false);
     }
 }
