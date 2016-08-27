a35d2f157f7811affcce215bc781854c8bf1c779
danmalafaia <danilo.fml9@gmail.com>
2016-08-27 14:42:44 -0300

    Separando - Curso, Disciplina e Resposta

---
+++ app/src/main/java/br/com/navi/enadumapp/Model/Curso.java
@@ -0,0 +1,42 @@
+package br.com.navi.enadumapp.Model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.navi.enadumapp.Model.Resultados.Disciplina;
+
+/**
+ * Created by Danilo on 27/08/2016.
+ */
+public class Curso implements Serializable {
+
+    private String nome;
+    private Long id;
+    private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+    public Curso(String nome, Long id){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
