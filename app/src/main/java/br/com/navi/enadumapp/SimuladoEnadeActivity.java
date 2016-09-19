ef83cd98a043abb5a9510cdd67924d08f8c30480
danmalafaia <leite.dfm@gmail.com>
2016-09-19 02:31:10 -0300

    Interacoes de inicio e fim de simulado, salvando as respostas para enviar para o servidor

--- app/src/main/java/br/com/navi/enadumapp/SimuladoEnadeActivity.java
+++ app/src/main/java/br/com/navi/enadumapp/SimuladoEnadeActivity.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import br.com.navi.enadumapp.Fragment.FragmentQuestaoEnade;
+import br.com.navi.enadumapp.Request.ResultadoRequest;
 import br.com.navi.enadumapp.Utils.SessionRepository;
+import br.com.navi.enadumapp.models.DTO.AlunoDTO;
+import br.com.navi.enadumapp.models.DTO.RespostaDTO;
+import br.com.navi.enadumapp.models.DTO.SimuladoDTO;
 import br.com.navi.enadumapp.models.SimuladoEnade;
 
 /**
@@ -25,6 +34,11 @@ public class SimuladoEnadeActivity extends AppCompatActivity {
     private Button btnNext;
     private Button btnPrev;
     private static int posicao;
+    private ResultadoRequest resultadoRequest;
+    private SimuladoDTO simuladoDTO;
+    private AlunoDTO alunoDTO;
+    private List<RespostaDTO> respostasDTO;
+    FragmentQuestaoEnade fragmentQuestaoEnade;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,42 +50,60 @@ public class SimuladoEnadeActivity extends AppCompatActivity {
         this.cronometro = (Chronometer) findViewById(R.id.chronometer2);
         this.btnNext = (Button) findViewById(R.id.simulado_btn_next);
         this.btnPrev = (Button) findViewById(R.id.simulado_btn_previous);
-
         this.curso.setText(simulado.getTitulo());
         this.cronometro.start();
 
-        FragmentQuestaoEnade fragmentQuestaoEnade = new FragmentQuestaoEnade();
+        simuladoDTO = new SimuladoDTO();
+        simuladoDTO.setId(simulado.getId());
+        alunoDTO = new AlunoDTO();
+        alunoDTO.setId(SessionRepository.aluno.getId());
+        respostasDTO = new LinkedList<RespostaDTO>();
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragmento,fragmentQuestaoEnade);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        moveQuestion();
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                posicao += 1;
-                FragmentQuestaoEnade fragmentQuestaoEnade = new FragmentQuestaoEnade();
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragmento,fragmentQuestaoEnade);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                RespostaDTO respostaDTO = new RespostaDTO();
+                respostaDTO.setId(fragmentQuestaoEnade.getIdResposta());
+                if(respostaDTO.getId() != null){
+                    respostasDTO.add(respostaDTO);
+                    Log.d("Teste", respostaDTO.getId().toString());
+                }
+                if (posicao == simulado.getQuestoes().size() - 1){
+                    Toast.makeText(SimuladoEnadeActivity.this,"Final do Simulado", Toast.LENGTH_SHORT).show();
+                } else {
+                    posicao += 1;
+                    moveQuestion();
+                }
             }
         });
 
         btnPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                posicao -= 1;
-                FragmentQuestaoEnade fragmentQuestaoEnade = new FragmentQuestaoEnade();
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragmento,fragmentQuestaoEnade);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                RespostaDTO respostaDTO = new RespostaDTO();
+                respostaDTO.setId(fragmentQuestaoEnade.getId()) ;
+                if (respostaDTO.getId() != null){
+                    respostasDTO.set(posicao,respostaDTO);
+                }
+                if (posicao == 0){
+                    Toast.makeText(SimuladoEnadeActivity.this,"Deseja sair do simulado",Toast.LENGTH_SHORT).show();
+                } else {
+                    posicao -= 1;
+                    moveQuestion();
+                }
             }
         });
+    }
+
+    private void moveQuestion() {
+        fragmentQuestaoEnade = new FragmentQuestaoEnade();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmento,fragmentQuestaoEnade);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     public SimuladoEnade getSimulado(){
---
+++ app/src/main/java/br/com/navi/enadumapp/models/DTO/SimuladoDTO.java
@@ -0,0 +1,21 @@
+package br.com.navi.enadumapp.models.DTO;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Danilo on 19/09/2016.
+ */
+
+public class SimuladoDTO {
+
+    @SerializedName("id")
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
